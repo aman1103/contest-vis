5294bcc9ebec65297450ef589972b27a7f9aabd7
Aman Gaharwar <amangaharwar1103@gmail.com>
2022-12-29 20:40:30 +0530

    Filtered Submission

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import axios from "axios";
 
 const App = () => {
   const [handle, setHandle] = useState("");
+  const [accepted, setAccepted] = useState([]);
   const handleSubmit = async () => {
     const response = await axios.get(
       `https://codeforces.com/api/user.status?handle=${handle.trim()}`
     );
     console.log(response.data);
+    const temp = response.data.result.filter(
+      (submission) => submission.verdict === "OK"
+    );
+    console.log(temp.length);
+    const set = new Set([]);
+    temp.map((submission) => {
+      set.add(submission.problem.name);
+    });
+    console.log(set.size);
+    setAccepted(
+      response.data.filter((submission) => submission.verdict === "OK")
+    );
   };
   return (
     <>
