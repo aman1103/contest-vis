ffe5cdf05d181f901d7223f5a373b8e476a254e6
Aman Gaharwar <amangaharwar1103@gmail.com>
2023-01-15 22:50:08 +0530

    Removed useCallback

--- src/FindDates.jsx
+++ src/FindDates.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeatMap from "./Charts/HeatMap";
 
 const FindDates = ({ data: initialData }) => {
